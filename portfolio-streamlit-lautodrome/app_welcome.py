7b5db7086b77e5a72efb9d8a160e0c136614d9d2
Benjamin Ragot <br.ragot@gmail.com>
2022-03-31 00:19:20 +0200

    updated the welcome page.

--- portfolio-streamlit-lautodrome/app_welcome.py
+++ portfolio-streamlit-lautodrome/app_welcome.py
@@ -7,5 +7,5 @@ def app():
     # columns[1].title("Portfolio de L'Autodrome")
     st.image('images/welcome_page/logo.png',width=200, use_column_width=False)
     st.title("Portfolio de L'Autodrome")
-    st.markdown('Un petit texte d\'intro ici !')
+    st.markdown('Bienvenue sur le site du compte Instagram [L\'Autodrome](https://www.instagram.com/lautodrome.hd/).')
     st.image('images/paintings_from_photos/miura.png', caption='A Lamborghini Miura by L\'Autodrome', use_column_width=False)
