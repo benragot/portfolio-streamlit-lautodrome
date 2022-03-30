b5bab19327ebfb7be6a4d6868e0a9fecd3b8c548
Benjamin Ragot <br.ragot@gmail.com>
2022-03-30 23:56:05 +0200

    removed the columns that use turtle.

--- portfolio-streamlit-lautodrome/app_welcome.py
+++ portfolio-streamlit-lautodrome/app_welcome.py
@@ -2,8 +2,8 @@ from turtle import width
 import streamlit as st
 
 def app():
-    columns = st.columns(2)
-    columns[0].image('images/welcome_page/logo.png',width=200, use_column_width=False)
-    columns[1].title("Portfolio de L'Autodrome")
+    # columns = st.columns(2)
+    # columns[0].image('images/welcome_page/logo.png',width=200, use_column_width=False)
+    # columns[1].title("Portfolio de L'Autodrome")
     st.markdown('Un petit texte d\'intro ici !')
     st.image('images/paintings_from_photos/miura.png',width=750, caption='A Lamborghini Miura', use_column_width=False)
